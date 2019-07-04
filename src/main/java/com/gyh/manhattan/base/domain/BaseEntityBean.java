81833c37a11ca23389eb7517e702240b6c27d21b
steve0517@163.com <steve0517@163.com>
2019-07-04 18:09:32 +0800

    新增实体类基类

---
+++ src/main/java/com/gyh/manhattan/base/domain/BaseEntityBean.java
@@ -0,0 +1,37 @@
+package com.gyh.manhattan.base.domain;
+
+import java.util.Date;
+
+/**
+ * @author gaoyuhang
+ */
+public class BaseEntityBean {
+
+    private Long id;
+    private String deletedFlag = "0";
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeletedFlag() {
+        return deletedFlag;
+    }
+
+    public void setDeletedFlag(String deletedFlag) {
+        this.deletedFlag = deletedFlag;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/com/gyh/manhattan/domain/SysMessage.java
+++ src/main/java/com/gyh/manhattan/domain/SysMessage.java
@@ -1,27 +1,14 @@
 package com.gyh.manhattan.domain;
 
-import java.util.Date;
+import com.gyh.manhattan.base.domain.BaseEntityBean;
 
-public class SysMessage {
-    private Long id;
+public class SysMessage extends BaseEntityBean {
 
     private String title;
 
     private String content;
 
     private Long userId;
-
-    private String deletedFlag = "0";
-
-    private Date createdAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getTitle() {
         return title;
@@ -45,21 +32,5 @@ public class SysMessage {
 
     public void setUserId(Long userId) {
         this.userId = userId;
-    }
-
-    public String getDeletedFlag() {
-        return deletedFlag;
-    }
-
-    public void setDeletedFlag(String deletedFlag) {
-        this.deletedFlag = deletedFlag;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
     }
 }
